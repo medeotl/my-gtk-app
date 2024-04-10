96e0d8098125ec30604c9cd2ce5bd011bacdb7eb
medeotl <fasiellon@yahoo.it>
2024-04-10 17:47:17 +0200

    Hello world 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{glib, Application};
+
+const APP_ID: &str = "org.gtk_rs.HelloWorld1";
+
+fn main() -> glib::ExitCode {
+    // Create a new application
+    let app = Application::builder()
+        .application_id(APP_ID)
+        .build();
+
+    // Run the application
+    app.run()
 }
