cfab17b1a9bf0e29b5e48c791fd7e02b40388ee4
medeotl <fasiellon@yahoo.it>
2024-04-11 22:02:43 +0200

    Hello world 3

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow};
+use gtk::{glib, Application, ApplicationWindow, Button};
 
-const APP_ID: &str = "org.gtk_rs.HelloWorld2";
+const APP_ID: &str = "org.gtk_rs.HelloWorld3";
 
 fn main() -> glib::ExitCode {
     // Create a new application
-    let app = Application::builder()
-        .application_id(APP_ID)
-        .build();
+    let app = Application::builder().application_id(APP_ID).build();
 
     // Connect to "activate" signal of 'app'
     app.connect_activate(build_ui);
@@ -17,13 +15,27 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    // Create a window and set the title
+    // Create a button with label and margins
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+    
+    // Connect to "clicked" signal of 'button'
+    button.connect_clicked(|button| {
+        // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label("Hello World!");
+    });
+    
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My Gtk App")
+        .child(&button)
         .build();
-        
+
     // Present window
     window.present();
-    
 }
