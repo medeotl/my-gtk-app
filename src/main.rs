b2480802c6853c15277538897ba65bffcbba7fc8
medeotl <fasiellon@yahoo.it>
2024-07-11 18:03:35 +0200

    Memory Managment

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button};
+use std::cell::Cell;
+use std::rc::Rc;
 
-const APP_ID: &str = "org.gtk_rs.HelloWorld3";
+use glib::clone;
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Button, Orientation};
+
+const APP_ID: &str = "org.gtk_rs.GObjectMemoryManagement5";
 
 fn main() -> glib::ExitCode {
     // Create a new application
@@ -15,27 +19,50 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    // Create a button with label and margins
-    let button = Button::builder()
-        .label("Press me!")
+    // Create two buttons
+    let button_increase = Button::builder()
+        .label("Lorenzo incrementa")
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .build();
-    
-    // Connect to "clicked" signal of 'button'
-    button.connect_clicked(|button| {
-        // Set the label to "Hello World!" after the button has been clicked on
-        button.set_label("Hello World!");
-    });
-    
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("My Gtk App")
-        .child(&button)
+    let button_decrease = Button::builder()
+        .label("Federico decrementa")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
         .build();
 
-    // Present window
+    // Reference-counted object with inner mutability
+    let number = Rc::new(Cell::new(0));
+
+    // Connect callbacks
+    // When a button is clicked, 'number' and label of the other button will be changed
+    button_increase.connect_clicked(clone!(@weak number, @weak button_decrease => move |_| {
+        number.set(number.get() + 1);
+        button_decrease.set_label(&number.get().to_string());
+    }));
+    button_decrease.connect_clicked(clone!(@weak button_increase => move |_| {
+        number.set(number.get() - 1);
+        button_increase.set_label(&number.get().to_string());
+    }));
+
+    // Add button to 'gtk_box'
+    let gtk_box = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+    gtk_box.append(&button_increase);
+    gtk_box.append(&button_decrease);
+
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Zio Nico GTK App")
+        .child(&gtk_box)
+        .build();
+
+    // Present the window
     window.present();
 }
